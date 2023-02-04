44398817923fd6679ce68719700fd472be5480f2
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-02-04 15:31:05 +0900

    change: configにバージョンを持たせるように変更

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,25 @@
 type Config = {
-	host: string;
-	i: string;
-	master?: string;
-	wsUrl: string;
-	apiUrl: string;
-	keywordEnabled: boolean;
-	reversiEnabled: boolean;
-	notingEnabled: boolean;
-	chartEnabled: boolean;
-	serverMonitoring: boolean;
-	mecab?: string;
-	mecabDic?: string;
-	memoryDir?: string;
-};
+	version: string,
+	host: string,
+	i: string,
+	master?: string,
+	wsUrl: string,
+	apiUrl: string,
+	keywordEnabled: boolean,
+	reversiEnabled: boolean,
+	notingEnabled: boolean,
+	chartEnabled: boolean,
+	serverMonitoring: boolean,
+	mecab?: string,
+	mecabDic?: string,
+	memoryDir?: string
+}
 
-const config = require('../config.json');
+const config = require('../config.json')
+const packageJson = require('../package.json')
 
-config.wsUrl = config.host.replace('http', 'ws');
-config.apiUrl = config.host + '/api';
+config.version = packageJson.version
+config.wsUrl = config.host.replace('http', 'ws')
+config.apiUrl = config.host + '/api'
 
-export default config as Config;
+export default config as Config
