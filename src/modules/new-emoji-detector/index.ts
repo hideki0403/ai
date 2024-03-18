863ad3939ce190c9ed2f9a47623ee19f03b9d717
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-03-18 14:49:11 +0900

    feat: 絵文字の一括通知に対応

--- src/modules/new-emoji-detector/index.ts
+++ src/modules/new-emoji-detector/index.ts
@@ -8,6 +8,8 @@ import config from '@/config.js'
 export default class extends Module {
 	public readonly name = 'new-emoji-detector'
 	private ignoreCategory: string[] = []
+	private bulkNotifyEmojis: string[] = []
+	private bulkNotifyCooldownTimer: NodeJS.Timeout | null = null
 
 	@bindThis
 	public install() {
@@ -21,8 +23,24 @@ export default class extends Module {
 	private async onEmojiAdded(emoji: Misskey.entities.EmojiDetailed) {
 		if (emoji.category && this.ignoreCategory.includes(emoji.category)) return
 		this.log(`New emoji detected: ${emoji.name}`)
+
+		if (config.newEmojiAnnouncementAtOnce) {
+			this.bulkNotifyEmojis.push(emoji.name)
+			if (this.bulkNotifyCooldownTimer) clearTimeout(this.bulkNotifyCooldownTimer)
+			this.bulkNotifyCooldownTimer = setTimeout(this.bulkNotify, 1000 * 60 * 3)
+		} else {
+			await this.aira.post({
+				text: serifs.newEmojiDetector.notify(emoji.name),
+			})
+		}
+	}
+
+	@bindThis
+	private async bulkNotify() {
+		this.bulkNotifyCooldownTimer = null
 		await this.aira.post({
-			text: serifs.newEmojiDetector.notify(emoji.name),
+			text: serifs.newEmojiDetector.bulkNotify(this.bulkNotifyEmojis),
 		})
+		this.bulkNotifyEmojis = []
 	}
 }
